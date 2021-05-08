59fe2e3d70e414e128b966532a782ec89601a5fa
Ricardo Ludwig <ricardo.ludwig@gmail.com>
2021-05-07 22:46:25 -0300

    Added help

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# go-translate
+# Translator
+
+Translator is a CLI application to translate words or sentences. I'm using this little programing to put into practice all the new stuff
+that I'm learning in Go.
+Translator uses [libreTranslate API](https://libretranslate.com/docs/), during my tests a got some instability issues, so is possible that happens occasionally.
+
+### Usage:
+
+```
+translator [command]
+
+Available commands:
+languages	 get available available languages to translate
+translate	 has three arguments source language, target language, and return (enter) command.
+                 after that, you can type a word or sentence to translate
+help		 show this
+
+Take a look at translator command, e.g.
+
+translator en pt [return]
+Translate
+hello
+Olá.
+```
--- translator.go
+++ translator.go
@@ -17,7 +17,18 @@ func main() {
 
 	args := os.Args[1:]
 
+	if len(args) < 1 {
+		fmt.Println("Error: no args found")
+		help()
+		os.Exit(1)
+	}
+
 	source := args[0]
+
+	if source == "help" {
+		help()
+		os.Exit(0)
+	}
 
 	if source == "languages" {
 		model.Print(languages)
@@ -35,6 +46,29 @@ func main() {
 
 	translated := translate(text, source, target)
 	fmt.Println(translated)
+
+}
+
+func help() {
+
+	fmt.Println("Translator is a CLI application to translate words or sentences.")
+	fmt.Println("I'm using this little programing to put into practice all the new stuff that I'm learning in Go.")
+	fmt.Print("Translator uses libreTranslate API, during my tests a got some instability issues,")
+	fmt.Println(" so is possible that happens occasionally.")
+	fmt.Println("Usage:")
+	fmt.Println("\t translator [command]")
+	fmt.Println("")
+	fmt.Println("Available commands:")
+	fmt.Println("\tlanguages\t get available available languages to translate")
+	fmt.Println("\ttranslate\t has three arguments source language, target language, and return (enter) command.")
+	fmt.Println("\t\t\t after that, you can type a word or sentence to translate")
+	fmt.Println("\thelp     \t show this")
+	fmt.Println("")
+	fmt.Println("Take a look at translator command, e.g.")
+	fmt.Println("\ttranslator en pt [return]")
+	fmt.Println("\tTranslate")
+	fmt.Println("\thello")
+	fmt.Println("\tOlá.")
 
 }
 
